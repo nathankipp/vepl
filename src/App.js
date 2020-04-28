fa3e73ef91c9205e448b789fefc6d5dfa9352500
Nathan Kipp <nathankipp@users.noreply.github.com>
2020-04-27 19:27:35 -0500

    react app and bulma (#1)
    * Initialize project using Create React App
    
    * clean out react app
    
    * clean out react app
    
    * favicon and public folder changes
    
    * set up bulma w/ App.scss file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.sass';
+
+function App() {
+  return (
+    <div className="has-text-info">
+      The future home of VEPL
+    </div>
+  );
+}
+
+export default App;
