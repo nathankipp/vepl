20d01cdaae1f11608126bfed56ad5363500c72de
Nathan Kipp <nathankipp@users.noreply.github.com>
2020-04-29 12:20:28 -0500

    mock up team cards (#2)
    * experiement w/ ui for team cards
    * team selection; active seasons & teams in table; needs refactor, badly:)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
 import React from 'react';
-import './App.sass';
+import TeamCard from './TeamCard.js';
 
-function App() {
-  return (
-    <div className="has-text-info">
-      The future home of VEPL
-    </div>
-  );
+import data from './seasons/data.json';
+const teams = data.map(({ name, shortName, seasons }) => ({
+  name,
+  shortName,
+  seasons
+}));
+
+class App extends React.Component {
+  state = {
+    teams,
+    selectedTeams: []
+  };
+
+  clickTeam = (team) => {
+    const { selectedTeams } = this.state;
+    const teams = new Set(selectedTeams);
+    if (teams.has(team)) {
+      teams.delete(team);
+    } else {
+      teams.add(team);
+    }
+    this.setState({ selectedTeams: Array.from(teams) });
+  }
+
+  render() {
+
+    const { teams, selectedTeams } = this.state;
+    const isSelected = team => selectedTeams.includes(team.shortName);
+
+    let activeSeasons = [];
+    teams
+      .filter(isSelected)
+      .forEach(team => {
+        if (activeSeasons.length === 0) {
+          activeSeasons = team.seasons;
+        } else {
+          activeSeasons = activeSeasons.filter(season => team.seasons.includes(season));
+        }
+      });
+    const playedAllActiveSeasons = seasons =>
+      activeSeasons.length === 0 || activeSeasons.filter(year => seasons.includes(year)).length;
+
+    return (
+      <div className="columns">
+      <section className="section column is-7">
+        <h3 className="is-size-3">Seasons in play</h3>
+        <p>{activeSeasons.join(', ')}</p>
+        <h3 className="is-size-3">Table</h3>
+        <pre>{selectedTeams.map(shortName => teams.find(t => t.shortName === shortName).name).join('\n')}</pre>
+      </section>
+      <section className="section column is-5 teams">
+      <h3 className="is-size-3">Choose Teams</h3>
+      {
+        teams.map((team) => {
+          const selected = isSelected(team);
+          const disabled = !playedAllActiveSeasons(team.seasons);
+          return (
+            <TeamCard
+              key={team.shortName}
+              team={team}
+              click={this.clickTeam}
+              selected={selected}
+              disabled={disabled}
+            />
+          );
+        })
+      }
+      </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/badges/index.js
@@ -0,0 +1,101 @@
+import ARS from './ARS.png';
+import AST from './AST.png';
+import BAR from './BAR.png';
+import BHA from './BHA.png';
+import BIR from './BIR.png';
+import BLB from './BLB.png';
+import BLP from './BLP.png';
+import BOL from './BOL.png';
+import BOU from './BOU.png';
+import BRA from './BRA.png';
+import BUR from './BUR.png';
+import CAR from './CAR.png';
+import CHA from './CHA.png';
+import CHE from './CHE.png';
+import COV from './COV.png';
+import CRY from './CRY.png';
+import DER from './DER.png';
+import EVE from './EVE.png';
+import FUL from './FUL.png';
+import HUD from './HUD.png';
+import HUL from './HUL.png';
+import IPS from './IPS.png';
+import LEE from './LEE.png';
+import LEI from './LEI.png';
+import LIV from './LIV.png';
+import MCI from './MCI.png';
+import MID from './MID.png';
+import MUN from './MUN.png';
+import NEW from './NEW.png';
+import NOR from './NOR.png';
+import NOT from './NOT.png';
+import OLD from './OLD.png';
+import POR from './POR.png';
+import QPR from './QPR.png';
+import REA from './REA.png';
+import SHU from './SHU.png';
+import SHW from './SHW.png';
+import SOU from './SOU.png';
+import STK from './STK.png';
+import SUN from './SUN.png';
+import SWA from './SWA.png';
+import SWI from './SWI.png';
+import TOT from './TOT.png';
+import WAT from './WAT.png';
+import WBA from './WBA.png';
+import WDN from './WDN.png';
+import WHU from './WHU.png';
+import WIG from './WIG.png';
+import WOL from './WOL.png';
+
+export default {
+  ARS,
+  AST,
+  BAR,
+  BHA,
+  BIR,
+  BLB,
+  BLP,
+  BOL,
+  BOU,
+  BRA,
+  BUR,
+  CAR,
+  CHA,
+  CHE,
+  COV,
+  CRY,
+  DER,
+  EVE,
+  FUL,
+  HUD,
+  HUL,
+  IPS,
+  LEE,
+  LEI,
+  LIV,
+  MCI,
+  MID,
+  MUN,
+  NEW,
+  NOR,
+  NOT,
+  OLD,
+  POR,
+  QPR,
+  REA,
+  SHU,
+  SHW,
+  SOU,
+  STK,
+  SUN,
+  SWA,
+  SWI,
+  TOT,
+  WAT,
+  WBA,
+  WDN,
+  WHU,
+  WIG,
+  WOL,
+};
